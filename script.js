ea5cbc33198726f8943715a2d8a73538430777c1
Jonathan Peters <jonathanpeters051@gmail.com>
2024-11-23 13:25:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ document.getElementById('hamburger').addEventListener('click', function() {
     const navLinks = document.getElementById('nav-links');
     navLinks.classList.toggle('active');
 });
+
+const whatsAppLink = document.querySelector('.whatsapp-link');
+
+whatsappLink.addEventListener('click', (e) => {
+e.preventDefault();
+const whatsappNumber = '27661199255'; 
+const message = 'Hello! ';
+Optional message
+const url = `(link unavailable)
+{encodeURIComponent(message)}`;
+window.open(url, '_blank'):
+});
